959c658398f59f64cacf6dbf998c1fb6303ba70a
akhileshmagdum <akhileshmagdum@gmail.com>
2023-07-03 17:26:14 +0530

    1.2-Right Triangle Pattern

--- pattern/PrintPattern.java
+++ pattern/PrintPattern.java
@@ -7,6 +7,7 @@ public class PrintPattern {
 
     public static void main(String[] args) {
         printSquare(5);
+        printRightTriangle(5);
     }
 
     /**
@@ -15,6 +16,18 @@ public class PrintPattern {
     static void printSquare(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * https://practice.geeksforgeeks.org/problems/right-triangle/1
+     */
+    static void printRightTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
