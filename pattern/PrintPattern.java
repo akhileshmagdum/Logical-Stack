48b56dcdce8ac60c811fd73fdec1c002992e7e3d
akhileshmagdum <akhileshmagdum@gmail.com>
2023-07-03 16:29:55 +0530

    1.2-Square Pattern

---
+++ pattern/PrintPattern.java
@@ -0,0 +1,23 @@
+package pattern;
+
+/**
+ * GFG Star (asterisk) Pattern related problem
+ */
+public class PrintPattern {
+
+    public static void main(String[] args) {
+        printSquare(5);
+    }
+
+    /**
+     * https://practice.geeksforgeeks.org/problems/square-pattern/1
+     */
+    static void printSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
