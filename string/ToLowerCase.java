9d01b015b11cd7f714a7781c1877bf841a360a76
akhileshmagdum <akhileshmagdum@gmail.com>
2023-08-05 13:16:26 +0530

    709-To Lower Case

---
+++ string/ToLowerCase.java
@@ -0,0 +1,15 @@
+package string;
+
+/**
+ * https://leetcode.com/problems/to-lower-case/
+ */
+public class ToLowerCase {
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
