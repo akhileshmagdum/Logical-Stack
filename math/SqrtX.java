c6ec59b2892d8beaaab0fb600e997ba7a63ff4e9
akhileshmagdum <akhileshmagdum@gmail.com>
2023-08-21 11:41:23 +0530

    69-Sqrt(x)

---
+++ math/SqrtX.java
@@ -0,0 +1,15 @@
+package math;
+
+/**
+ * https://leetcode.com/problems/sqrtx/
+ */
+public class SqrtX {
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(10));
+    }
+
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
