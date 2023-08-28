52edb66a653a66a4a16c8c86ee1f1c75c9635c2f
akhileshmagdum <akhileshmagdum@gmail.com>
2023-08-28 18:20:43 +0530

    66-Plus One

---
+++ array/PlusOne.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/plus-one/
+ */
+public class PlusOne {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
