f91c64d01ad185a967546073c432e7717fe5b424
akhileshmagdum <akhileshmagdum@gmail.com>
2023-08-30 10:42:06 +0530

    1929-Concatenation of Array

---
+++ array/ConcatenationOfArray.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/concatenation-of-array/
+ */
+public class ConcatenationOfArray {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getConcatenation(new int[]{1, 2, 1})));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
