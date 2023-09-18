907ded4340e1b950648adbccabacbb266718e197
akhileshmagdum <akhileshmagdum@gmail.com>
2023-09-18 10:15:13 +0530

    2235-Add Two Integers

---
+++ math/AddTwoIntegers.java
@@ -0,0 +1,15 @@
+package math;
+
+/**
+ * https://leetcode.com/problems/add-two-integers/
+ */
+public class AddTwoIntegers {
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
